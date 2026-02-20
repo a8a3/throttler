e4ee102097bf551d4b5f1a13e3c82958b009ddc9
a8a3 <you@example.com>
2026-02-20 21:17:46 +0100

    isRunning is plain bool protected by mutex

--- include/throttler.hpp
+++ include/throttler.hpp
@@ -24,10 +24,11 @@ public:
 
         tokenCreatorThread_ = std::thread{
             [this, tokenAddInterval, maxTokens] () {
-                while (isRunning_.load()) {
+                while (true) {
                     std::this_thread::sleep_for(tokenAddInterval);
                     {
                         std::unique_lock lock{mtx_};
+                        if (!isRunning_) break; // while
                         currentTokensNum_ = std::min(maxTokens, currentTokensNum_ + 1);
                     }
                     cv_.notify_one();
@@ -40,7 +41,10 @@ public:
     Throttler& operator=(const Throttler&) = delete;
 
     ~Throttler() {
-        isRunning_.store(false);
+        {
+            std::unique_lock lock{mtx_};
+            isRunning_ = false;
+        }
         cv_.notify_all();
 
         if (tokenCreatorThread_.joinable()) 
@@ -51,14 +55,14 @@ public:
         // если есть свободные токены- взять один и продолжить выполнение,
         // если нет- ждать пока не появится
         std::unique_lock lock{mtx_};
-        cv_.wait(lock, [this] () {return currentTokensNum_ > 0 || !isRunning_.load();});
-        if (!isRunning_.load()) return;
+        cv_.wait(lock, [this] () {return currentTokensNum_ > 0 || !isRunning_;});
+        if (!isRunning_) return;
         --currentTokensNum_;
      }
 
 private:
     std::thread tokenCreatorThread_;
-    std::atomic_bool isRunning_{true};
+    bool isRunning_{true};
 
     std::mutex mtx_;
     std::condition_variable cv_;
